f780244fb25f3c3288b5f1bb806565b17e29c408
Binayak Mondal <76868488+binayak9932@users.noreply.github.com>
2022-01-28 04:48:08 +0530

    Add files via upload

---
+++ icon.py
@@ -0,0 +1,20 @@
+import  winreg
+
+path = winreg.HKEY_LOCAL_MACHINE
+
+def save_reg(k = '29', v ='%windir%\System32\shell32.dll,-50'):
+    try:
+        key = winreg.OpenKeyEx(path, r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\")
+        newKey = winreg.CreateKey(key,"Shell Icons")
+        winreg.SetValueEx(newKey, k, 0, winreg.REG_SZ, str(v))
+        if newKey:
+            winreg.CloseKey(newKey)
+            print("Done sucessfully!!!!!")
+        return True
+    except Exception as e:
+        print(e)
+        print("ERROR!!!!!")
+    return False
+
+
+save_reg()
